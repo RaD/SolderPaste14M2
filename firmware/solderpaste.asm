8f0e6c1287dea17b9891c9d8b5899edf78966dfe
Ruslan Popov <ruslan.popov@gmail.com>
2014-08-31 20:32:32 +0400

    Make motor rotating.

--- firmware/solderpaste.asm
+++ firmware/solderpaste.asm
@@ -4,6 +4,16 @@
 ; * http://habrahabr.ru/post/110894/
 ;
 
+; Internal RC oscillator at 9.6MHz; start-up time 14CK+64ms
+; CKSEL=10, SUT=10
+
+; Using this fuses chip works on 9.6MHz with HW divider by 8,
+; so we get 1.2MHz, also we can program internal clock
+; prescaler to divide this frequency by 128 (see CLKPR).
+; Finally, MCU will work on 9375kHz.
+; Be careful, while we use prescaler, so programmer hardware
+; will work with chip only on 9.6MHz/128 = 75kHz.
+
 .include "tn13def.inc"
 
 ; Port usage
@@ -11,7 +21,6 @@
 ; PB4   - control button
 ; PB5   - mode button
 
-; Set FUSES: CKSEL1=0, CKSEL0=1 (4800KhZ system clock)
 
 ; Register defines:
 .def	TMP		= R21
@@ -23,7 +32,8 @@
 
 .equ    MOTOR_BIT = 0 ; if set, motor is running
 .equ    DIR_BIT   = 1 ; if set, rotating left
-
+.equ    PAUSE     = 0xEA ; it is about 100ms for current prescaler
+                        ; 9375kHz/10/4 (ticks per delay loop)
 
 ;;; Write to port macro.
 .macro outm
@@ -77,7 +87,6 @@ PC_INTO0:
         ; finish interrupt
         outm    GIFR, (1<<PCIF)
 
-
 		pop		TMP
 
         ; dirty hack :)
@@ -97,27 +106,28 @@ RESET:
 		; stack init
         outm    SPL, low(RAMEND)
 
+        ; slowdown the MCU
+        outm    CLKPR, (1<<CLKPCE)  ; activate prescaler change
+        outm    CLKPR, (1<<CLKPS2) | (1<<CLKPS1) | (1<<CLKPS0) ; /128
+
+        ldi     CHANGE, (1<<DIR_BIT) ; change mask
 
 		; check the reset type
 		in		TMP, MCUSR
-		cpi		TMP, (1<<EXTRF)
-		breq	MODE_SWITCH
+		cpi 	TMP, (1<<EXTRF)
+        breq    MODE_SWITCH
+
+        ldi     STATE, (1<<DIR_BIT) ; rotating
+
+MODE_SWITCH:
+        eor     STATE, CHANGE
+		outm	MCUSR, 0    ; clearing is mandatory
 
 		; set initial values
 		ldi		SHIFT, 0b00110011   ; shift value
 		ldi		MASK, 0b00001111    ; shift mask
-        ldi     DELAY, 0xFF
-        ldi     STATE, (1<<DIR_BIT) ; initial
-        mov     CHANGE, STATE       ; register is needed for EOR
+        ldi     DELAY, PAUSE
 
-
-MODE_SWITCH:
-		; switch current mode
-        ; 0 x 1 => 1, 1 x 1 => 0
-		eor		STATE, CHANGE
-
-		; clear MCUSR register after Reset
-		outm	MCUSR, 0
 
 		; setup interrupt
 		outm	GIMSK, (1<<PCIE)	; enable Pin Change interrupt
@@ -141,8 +151,27 @@ MAIN:
         sbrc    STATE, MOTOR_BIT    ; skip if bit cleared
         rcall   ROTATION
 		;sleep
-        rcall   DELAYING
 		rjmp	MAIN
+
+
+;;; Function checks the direction and rotate motor by one
+;;; step in particular direction.
+ROTATION:
+        sbrc    STATE, DIR_BIT      ; skip if bit cleared
+        rjmp    MOVE_RIGHT
+
+        rolm    SHIFT
+		rjmp	ROTATE
+
+MOVE_RIGHT:
+        rorm    SHIFT
+
+ROTATE:
+		mov		TMP, SHIFT
+		and		TMP, MASK
+		out     PORTB, TMP
+        rcall   DELAYING
+        ret
 
 
 ;;; Delays execution
@@ -157,23 +186,5 @@ DELAY_LOOP:
         pop     TMP
         ret
 
-
-;;; Function checks the direction and rotate motor by one
-;;; step in particular direction.
-ROTATION:
-        sbrc    STATE, DIR_BIT  ; skip if bit cleared
-        rjmp    MOVE_RIGHT
-
-        rolm    SHIFT
-		rjmp	ROTATE
-
-MOVE_RIGHT:
-        rorm    SHIFT
-
-ROTATE:
-		mov		TMP, SHIFT
-		and		TMP, MASK
-		out     PORTB, TMP
-        ret
 
 .exit
